e7220e36a5931703599f7396e6511b677d0f331f
Layan-Dev <layanalghamdibusiness@hotmail.com>
2023-12-05 15:43:23 +0300

    fix config

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,4 +5,12 @@ export const dev = {
     port: process.env.SERVER_PORT,
     db: process.env.ATLAS_URL,
   },
+  email: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+    domain: process.env.EMAIL_ACTIVATION_DOMAIN,
+  },
+  auth: {
+    secretToken: process.env.SECRET_TOKEN,
+  },
 }
